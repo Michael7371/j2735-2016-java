194dc3cd9fafae00805195b36920df04d1d3dcb1
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2024-12-02 03:03:28 -0700

    TIM tests

--- src/test/java/j2735/DSRC/TIMMessageFrameTest.java
+++ src/test/java/j2735/DSRC/TIMMessageFrameTest.java
@@ -51,19 +51,19 @@ public class TIMMessageFrameTest extends BaseSerializeTest<TravelerInformationMe
         assertThat(xml, notNullValue());
     }
 
-//    @Test
-//    public void xmlDeserialize_generatedXml() throws IOException {
-//        TravelerInformationMessageFrame tim = fromXml(loadResource("/GeneratedTim.xml"));
-//        assertThat(tim, notNullValue());
-//        assertThat(tim, hasProperty("messageId", equalTo(new DSRCmsgID(31))));
-//    }
-//
-//    @Test
-//    public void xmlDeserialize_generatedXmlWithComputedLanes() throws IOException {
-//        TravelerInformationMessageFrame tim = fromXml(loadResource("/GeneratedTimWithComputedLanes.xml"));
-//        assertThat(tim, notNullValue());
-//        assertThat(tim, hasProperty("messageId", equalTo(new DSRCmsgID(31))));
-//    }
+    @Test
+    public void xmlDeserialize_generatedXml() throws IOException {
+        TravelerInformationMessageFrame tim = fromXml(loadResource("/GeneratedTim.xml"));
+        assertThat(tim, notNullValue());
+        assertThat(tim, hasProperty("messageId", equalTo(new DSRCmsgID(31))));
+    }
+
+    @Test
+    public void xmlDeserialize_generatedXmlWithComputedLanes() throws IOException {
+        TravelerInformationMessageFrame tim = fromXml(loadResource("/GeneratedTimWithComputedLanes.xml"));
+        assertThat(tim, notNullValue());
+        assertThat(tim, hasProperty("messageId", equalTo(new DSRCmsgID(31))));
+    }
 
     public final static String expectedXml = """
 <MessageFrame>
