5c6bbc50a67c0074106e1c4452000109478164d4
Matt Cook <matt.cook@trihydro.com>
2024-12-09 11:51:33 -0700

    swap System.out and System.err calls for standard logger calls

--- src/main/java/us/dot/its/jpo/ode/plugin/types/Asn1Choice.java
+++ src/main/java/us/dot/its/jpo/ode/plugin/types/Asn1Choice.java
@@ -1,11 +1,14 @@
 package us.dot.its.jpo.ode.plugin.types;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import us.dot.its.jpo.ode.plugin.serialization.SerializationUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public abstract class Asn1Choice implements Asn1Type {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     final boolean hasExtensionMarker;
 
     public Asn1Choice(boolean hasExtensionMarker) {
@@ -18,7 +21,7 @@ public abstract class Asn1Choice implements Asn1Type {
         try {
             return mapper.writeValueAsString(this);
         } catch (JsonProcessingException e) {
-            System.err.println(e.getMessage());
+            logger.error("Failed to covert value to json string",e);
             return "";
         }
     }
